3ec1524ebc8b1d538d4a011a0c2f54e13298f7cf
CrzyPr1est <dimka9604@gmail.com>
2017-02-14 10:18:28 +0300

    Data context and connection string

---
+++ HelpDesk/Models/DAL/HelpdeskContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HelpDesk.Models.DAL
+{
+    public class HelpdeskContext : DbContext
+    {
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Lifecycle> Lifecycles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Activ> Activs { get; set; }
+    }
+}
