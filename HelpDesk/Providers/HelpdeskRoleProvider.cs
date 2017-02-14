823dedaec2eae09ba9b967f2d415244ca189a65d
CrzyPr1est <dimka9604@gmail.com>
2017-02-14 10:34:34 +0300

    Role provider created

---
+++ HelpDesk/Providers/HelpdeskRoleProvider.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.Helpers;
+using System.Web.WebPages;
+using HelpDesk.Models;
+using HelpDesk.Models.DAL;
+
+namespace HelpDesk.Providers
+{
+    public class HelpdeskRoleProvider : RoleProvider
+    {
+        public override string[] GetRolesForUser(string login)
+        {
+            string[] role = new string[] { };
+            using (HelpdeskContext _db = new HelpdeskContext())
+            {
+                try
+                {
+                    // Получаем пользователя
+                    User user = (from u in _db.Users
+                                 where u.Login == login
+                                 select u).FirstOrDefault();
+                    if (user != null)
+                    {
+                        // получаем роль
+                        Role userRole = _db.Roles.Find(user.RoleId);
+
+                        if (userRole != null)
+                        {
+                            role = new string[] { userRole.Name };
+                        }
+                    }
+                }
+                catch
+                {
+                    role = new string[] { };
+                }
+            }
+            return role;
+        }
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            bool outputResult = false;
+            // Находим пользователя
+            using (HelpdeskContext _db = new HelpdeskContext())
+            {
+                try
+                {
+                    // Получаем пользователя
+                    User user = (from u in _db.Users
+                                 where u.Login == username
+                                 select u).FirstOrDefault();
+                    if (user != null)
+                    {
+                        // получаем роль
+                        Role userRole = _db.Roles.Find(user.RoleId);
+
+                        //сравниваем
+                        if (userRole != null && userRole.Name == roleName)
+                        {
+                            outputResult = true;
+                        }
+                    }
+                }
+                catch
+                {
+                    outputResult = false;
+                }
+            }
+            return outputResult;
+        }
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
