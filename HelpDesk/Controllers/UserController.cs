94b7b1539fece53cfa5b10acc9b87b48a04a36c4
CrzyPr1est <dimka9604@gmail.com>
2017-02-15 12:18:27 +0300

    Added ediditng and deleting methods

--- HelpDesk/Controllers/UserController.cs
+++ HelpDesk/Controllers/UserController.cs
@@ -33,6 +33,7 @@ namespace HelpDesk.Controllers
             return View();
         }
 
+        // Тот-же метод для создания
         [HttpPost]
         [Authorize(Roles = "Администратор")]
         public ActionResult Create(User user)
@@ -49,6 +50,48 @@ namespace HelpDesk.Controllers
             ViewBag.Roles = roles;
 
             return View(user);
+        }
+
+        // Метод для отображения информации для редактирования
+        [HttpGet]
+        [Authorize(Roles = "Администратор")]
+        public ActionResult Edit(int id)
+        {
+            User user = _db.Users.Find(id);
+            SelectList departments = new SelectList(_db.Departments, "Id", "Name", user.DepartmentId);
+            ViewBag.Departments = departments;
+            SelectList roles = new SelectList(_db.Roles, "Id", "Name", user.RoleId);
+            ViewBag.Roles = roles;
+
+            return View(user);
+        }
+
+        // Тот-же метод для редактирования
+        [HttpPost]
+        [Authorize(Roles = "Администратор")]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(user).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            SelectList departments = new SelectList(_db.Departments, "Id", "Name");
+            ViewBag.Departments = departments;
+            SelectList roles = new SelectList(_db.Roles, "Id", "Name");
+            ViewBag.Roles = roles;
+
+            return View(user);
+        }
+        [Authorize(Roles = "Администратор")]
+        public ActionResult Delete(int id)
+        {
+            User user = _db.Users.Find(id);
+            _db.Users.Remove(user);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
